5606ab21bc22dced8fe4fed89669475374c25bb2
appusdevis <appusdevis.k@tigeranalytics.com>
2025-05-21 14:55:50 +0530

    cors changes

--- src/main/java/com/tiger/capstone/config/WebConfig.java
+++ src/main/java/com/tiger/capstone/config/WebConfig.java
@@ -1,5 +1,4 @@
 package com.tiger.capstone.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")            // allow all origins
-            .allowedMethods("*")            // allow all HTTP methods (GET, POST, etc.)
-            .allowedHeaders("*")            // allow all headers
-            .allowCredentials(true)         // allow credentials (cookies, auth headers)
-            .maxAge(3600);
+            .allowedOriginPatterns("https://my-react-app1-fdekf4d5brcnaah6.canadacentral-01.azurewebsites.net/") // Replace with frontend URL
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
